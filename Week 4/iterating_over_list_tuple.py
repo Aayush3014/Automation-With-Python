3bc7a5b620a6808240fc41649eda619034dd70d1
Aayush3014 <0201csai036@niet.co.in>
2022-10-06 22:47:13 +0530

    Commit

---
+++ Week 4/iterating_over_list_tuple.py
@@ -0,0 +1,12 @@
+# Try out the enumerate function for yourself in this quick exercise. Complete the skip_elements function to return
+#  every other element from the 
+# list, this time using the enumerate function to check if an element is in an even position or an odd position.
+
+
+# def skip_elements(elements):
+# 	# code goes here
+	
+# 	return ___
+
+# print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
+# print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
